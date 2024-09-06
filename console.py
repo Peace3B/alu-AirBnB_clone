dc8f6dc6568109172613ad65296f06c79e85416e
Peace3B <p.keza@alustudent.com>
2024-09-06 12:34:31 +0000

    Yolla

---
+++ console.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"Entry point of the command interpreter"
+import cmd
+import models
+from models.user import User
+from models.amenity import Amenity 
+from models.city import City
+from models.place import Place
+from models.review import Review
+from models.state import State
+
+
