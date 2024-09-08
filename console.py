0ba35e8664df6e12b5170a3f6669bba91634609f
Peace3B <p.keza@alustudent.com>
2024-09-08 16:34:41 +0000

    Console

--- console.py
+++ console.py
@@ -6,16 +6,3 @@ from models.base_model import BaseModel
 from models import storage
 import re
 import json
-
-
-class HBNBCommand(cmd.Cmd):
-
-"""Class for the command interpreter."""
-
-prompt= "(hbnb)"
-
-def default(self, line):
-"""Catch commands if nothing else matches then."""
-# print("DEF:::", line)
-self._precmd(line)
-
