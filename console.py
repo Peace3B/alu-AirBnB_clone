66aec42a8987340239bb9a12f9b38901126d9ec7
Peace3B <p.keza@alustudent.com>
2024-09-08 16:41:07 +0000

    Console

--- console.py
+++ console.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """Module for the entry point of the command interpreter"""
 
-import cmd 
+import cmd
 from models.base_model import BaseModel
 from models import storage
 import re
