82b5812c8912891bb9b5045bd04ff1d4db85032e
Peace3B <p.keza@alustudent.com>
2024-09-08 15:47:20 +0000

    Base models

---
+++ models/base_model.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Module for Base class
+Contains the Base class for the AirBnB clone console
+"""
+
+import uuid
+from datetime import datetime
+from models import storage
+
+
