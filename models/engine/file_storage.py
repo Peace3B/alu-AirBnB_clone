bff4f77ce20c62cf5f251a0037a6de797d375e41
Peace3B <p.keza@alustudent.com>
2024-09-09 10:30:13 +0000

    file Storage

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -4,3 +4,32 @@
 import datetime
 import json
 import os
+
+class FileStorage:
+    """Serializes instances to a JSON file and deserializes JSON file to instances."""
+    
+    __file_path = "file.json"
+    __objects = {}
+
+    def all(self):
+        """Returns the dictionary __objects."""
+        return self.__objects
+
+    def new(self, obj):
+        """Sets in __objects the obj with key <obj class name>.id."""
+        key = f"{obj.__class__.__name__}.{obj.id}"
+        self.__objects[key] = obj
+
+    def save(self):
+        """Serializes __objects to the JSON file."""
+        with open(self.__file_path, 'w') as f:
+            json.dump({k: v.to_dict() for k, v in self.__objects.items()}, f)
+
+    def reload(self):
+        """Deserializes the JSON file to __objects, if it exists."""
+        try:
+            with open(self.__file_path, 'r') as f:
+                data = json.load(f)
+                # Assuming there's a logic to recreate instances from dict data
+        except FileNotFoundError:
+            pass
