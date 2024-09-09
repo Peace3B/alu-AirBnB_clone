6f930cb61b4e0218997fd7f1be7473aad7f57b4c
Peace3B <p.keza@alustudent.com>
2024-09-09 11:18:15 +0000

    Amenity

---
+++ tests/test_models/test_amenity.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Unittest module for Amenity class."""
+
+from models.amenity import Amenity
+from models.engine.file_storage import FileStorage
+from models.base_model import BaseModel
+from datetime import datetime
+from models import storage
+import json
+import re
+import os
+import time
+import unittest
