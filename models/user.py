ded64a7e23ca873c198c0062d437775d43640700
Peace3B <149768110+Peace3B@users.noreply.github.com>
2024-09-12 16:27:03 +0200

    Update user.py

--- models/user.py
+++ models/user.py
@@ -1,4 +1,10 @@
 #!/usr/bin/python3
-"""Module for User class."""
-
 from models.base_model import BaseModel
+
+
+class User(BaseModel):
+    """User class"""
+    email = ""
+    password = ""
+    first_name = ""
+    last_name = ""
