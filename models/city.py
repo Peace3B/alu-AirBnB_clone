e0e873d5fe2779ebd47159de0ff6d3b781e7614c
Peace3B <149768110+Peace3B@users.noreply.github.com>
2024-09-12 16:29:54 +0200

    Update city.py

--- models/city.py
+++ models/city.py
@@ -1,4 +1,15 @@
 #!/usr/bin/python3
-"""Module for City class."""
+
+"""This file defines the City Model
+It inherits from the BaseModel
+"""
 
 from models.base_model import BaseModel
+
+
+class City(BaseModel):
+    """The City Model"""
+
+    # Attributes
+    name = ""
+    state_id = ""
