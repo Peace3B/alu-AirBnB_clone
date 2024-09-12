c23ee15cf5947a17eedf3b6acf24752ae398b141
Peace3B <149768110+Peace3B@users.noreply.github.com>
2024-09-12 17:48:04 +0200

    Update file_storage.py

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -15,6 +15,7 @@ from models.amenity import Amenity
 from models.place import Place
 from models.review import Review
 from datetime import datetime
+from exceptions import InstanceNotFoundError, ModelNotFoundError
 
 
 class FileStorage:
