30c4ba593809be7e44ddbe243c67fd3d955d7b27
Peace3B <149768110+Peace3B@users.noreply.github.com>
2024-09-12 17:45:15 +0200

    Create exceptions.py

---
+++ exceptions.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""Module created to have a better arrangement of my codes."""
+
+# exceptions.py
+
+class InstanceNotFoundError(Exception):
+    """Exception raised when an instance is not found."""
+    pass
+
+class ModelNotFoundError(Exception):
+    """Exception raised when a model is not found."""
+    pass
