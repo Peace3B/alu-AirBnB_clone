a945d1f08fafdf140d3472b4cc90887c9d2f93b9
Peace3B <149768110+Peace3B@users.noreply.github.com>
2024-09-12 16:28:34 +0200

    Update review.py

--- models/review.py
+++ models/review.py
@@ -1,4 +1,16 @@
 #!/usr/bin/python3
-"""Module for Review class."""
+
+"""This file defines the Review Model
+It inherits from the BaseModel
+"""
 
 from models.base_model import BaseModel
+
+
+class Review(BaseModel):
+    """The Review Model"""
+
+    # Attributes
+    place_id = ""
+    user_id = ""
+    text = ""
