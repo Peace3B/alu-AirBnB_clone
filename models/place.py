de8970962a36414c5c19384d04cde24cb699d33b
Peace3B <149768110+Peace3B@users.noreply.github.com>
2024-09-12 16:29:23 +0200

    Update place.py

--- models/place.py
+++ models/place.py
@@ -1,4 +1,25 @@
 #!/usr/bin/python3
-"""Module for Place class."""
+
+"""This file defines the Place Model
+It inherits from the BaseModel
+"""
 
 from models.base_model import BaseModel
+from typing import List
+
+
+class Place(BaseModel):
+    """The Place Model"""
+
+    # Attributes
+    city_id = ""
+    user_id = ""
+    name = ""
+    description = ""
+    number_rooms = 0
+    number_bathrooms = 0
+    max_guest = 0
+    price_by_night = 0
+    latitude = 0.0
+    longitude = 0.0
+    amenity_ids = []
